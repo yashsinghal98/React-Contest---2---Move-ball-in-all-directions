f8a088eb27cd60344bdba6a88e4c7934c7533e92
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-10-25 18:34:52 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,49 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
-
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    let temp={...ballPosition};
+    temp.left=`0px`;
+    temp.top='0px';
+    setBallPosition(temp);
+  };
+  const handlekey=function(event){
+    console.log(event.key);
+    let temp={...ballPosition};
+    let valx=x;
+    let valy=y;
+    if(event.key==="ArrowRight")
+    {
+      temp.left=`${valx+5}px`;
+    }
+    else if(event.key==="ArrowDown")
+    {
+      temp.top=`${valy+5}px`;
+    }
+    else if(event.key==="ArrowUp")
+    {
+      temp.top=`${valy-5}px`;
+    }
+    else if(event.key==="ArrowLeft")
+    {
+      temp.left=`${valx-5}px`;
+    }
+    setBallPosition(temp);
+    setX(valx);
+    setY(valy);
+    console.log(ballPosition);
+}
+  const buttonClickHandler=()=>{
+    setRenderBall(true);
+    document.addEventListener("keydown",()=>handlekey(event))
+  }
+  const renderChoice = () => {
+    if(renderBall){return <div className="ball" style={ballPosition}></div>}
+		 else   return <button onClick={buttonClickHandler} >Click For One Ball</button>
+  };
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
